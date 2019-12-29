c64454a581cd7b30c27d7fc9d7d1b1fda4d64328
Darshan Gandhi <44197177+darshan-gandhi@users.noreply.github.com>
2019-12-29 13:02:08 +0530

    Add files via upload

---
+++ Face-Recognizer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+get_ipython().system('pip install --upgrade pip')
+get_ipython().system('pip install opencv-python')
+
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+# Read the input image
+img = cv2.imread('Test.jpeg')
+# Convert into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# Detect faces
+faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+# Draw rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+# Display the output
+cv2.imshow('img', img)
+cv2.waitKey()
+
+
+# In[ ]:
+
+
+
+
